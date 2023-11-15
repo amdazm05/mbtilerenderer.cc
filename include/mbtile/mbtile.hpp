3d49592fc84646319128c51c566e23e4cefa8953
amdazm05 <amdali1998@yahoo.com>
2023-11-15 18:54:04 +0500

    tests: added sql queries to search image blobs in mb tiles

--- include/mbtile/mbtile.hpp
+++ include/mbtile/mbtile.hpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <memory>
 #include <vector>
+#include <utility>
 
 namespace mbtile
 {
@@ -16,16 +17,6 @@ namespace mbtile
         public:
             /// @brief Constructors
             tile_t():tile_t(0,0){}
-            tile_t(tile_t &&t) noexcept: 
-                tile_col(std::exchange(t.tile_col,0)),
-                tile_row(std::exchange(t.tile_row,0)),
-                bitmap_image(t.bitmap_image)
-            {}
-            tile_t(tile_t &t) noexcept: 
-                tile_col(t.tile_col),
-                tile_row(t.tile_row)
-                bitmap_image(t.bitmap_image)
-            {}
             tile_t(std::size_t tile_col,std::size_t tile_row):
                 tile_col(tile_col),
                 tile_row(tile_row)
@@ -39,19 +30,9 @@ namespace mbtile
             std::size_t zoom_level;
         public:
             /// @brief Constructors
-            tilezoom_t():tile_t(0,0){}
-            tilezoom_t(tilezoom_t &&t) noexcept: 
-                zoom_level(std::exchange(t.tile_col,0)),
-                tiles(t.tiles)
-            {}
-            tilezoom_t(tilezoom_t &t) noexcept: 
-                zoom_level(t.zoom_level),
-                tiles(t.tiles)
-            {}
+            tilezoom_t():tilezoom_t(0){}
             tilezoom_t(std::size_t zoom_level):
-                zoom_level(zoom_level),
-                tile_row(tile_row)
-            {}
+                zoom_level(zoom_level){}
     };
 }
 
